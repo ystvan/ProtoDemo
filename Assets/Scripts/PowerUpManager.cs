0622a5cca23a8b5b72abbd022cd8b3debf33982a
ystvan <istvan.marki@gmail.com>
2016-09-14 10:53:29 +0200

    PowerUp
    controller & counter fixed

--- Assets/Scripts/PowerUpManager.cs
+++ Assets/Scripts/PowerUpManager.cs
@@ -7,8 +7,11 @@ public class PowerUpManager : MonoBehaviour
     int Timer;
     public static int PowerUpMeter;
 
+    public bool IsPowered;
+
     void Start()
-	{
+    {
+        IsPowered = false;
 	    Timer = 1;
 	    PowerUpMeter = 50; //Default bonus on start
 	}
@@ -26,6 +29,26 @@ public class PowerUpManager : MonoBehaviour
 
             GameObject.Instantiate(PowerUpPrefab, randomSpawn, transform.rotation);
         }
+
+	    if (Input.GetKeyDown(KeyCode.Z) && !IsPowered && PowerUpMeter >0)
+	    {
+            //Startup PowerUp Mode
+            IsPowered = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Z) && IsPowered)
+	    {
+            //Cancel PowerUp Mode
+            IsPowered = false;
+        }
+	    if (IsPowered)
+	    {
+	        PowerUpMeter--;
+	        if (PowerUpMeter == 0)
+	        {
+	            IsPowered = false;
+	        }
+            Debug.Log(PowerUpMeter);
+	    }
 	    Timer++;
 	}
 }
